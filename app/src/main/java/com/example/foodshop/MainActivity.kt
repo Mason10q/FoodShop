53db8d2f109e5a86c7e2c261a29e6824bb9b11b3
mason10q <moskvichev610@gmail.com>
2023-06-08 18:29:11 +0300

    Вёрстка для MainActivity и начало навигации

--- app/src/main/java/com/example/foodshop/MainActivity.kt
+++ app/src/main/java/com/example/foodshop/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.foodshop
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.foodshop.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.host_fragment) as NavHostFragment
+
+
+        val navController = navHostFragment.navController
+
+        binding.hostFragment
+        binding.bottomNavView.setupWithNavController(navController)
     }
 }
